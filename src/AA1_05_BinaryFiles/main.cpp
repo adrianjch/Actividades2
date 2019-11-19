4bdf3d1e95dabfa5b2e87852f2a439b4499aae13
Adrià Jiménez Chincolla <adrianjimenezchincolla@gmail.com>
2019-11-19 19:21:39 +0100

    Finished AA1_05

--- src/AA1_05_BinaryFiles/main.cpp
+++ src/AA1_05_BinaryFiles/main.cpp
@@ -3,21 +3,20 @@
 #include <fstream>
 #include <iostream>
 
-struct object {
-
+struct Object {
+	char type;
+	int x;
+	int y;
 };
 
 void SaveVector(std::vector<int> o, std::string fileName) {
-	int* vec = new int[o.size()];
-	for (int i = 0; i < o.size(); i++)
-		vec[i] = o[i];
-	delete[] vec;
 	std::ofstream myFile;
 	myFile.open(fileName, std::ios::binary);
 	if (myFile.is_open()) {
-		size_t len = sizeof(int)*o.size();
-		myFile.write(reinterpret_cast<char*>(&len), sizeof(size_t));
-		myFile.write(reinterpret_cast<char*>(&vec), len);
+		int len = o.size();
+		myFile.write(reinterpret_cast<char*>(&len), sizeof(int));
+		for (int i = 0; i < o.size(); i++)
+			myFile.write(reinterpret_cast<char*>(&o[i]), sizeof(int));
 	}
 	myFile.close();
 }
@@ -26,27 +25,63 @@ void RecoverVector(std::vector<int> &o, std::string fileName) {
 	std::ifstream myFile;
 	myFile.open(fileName, std::ios::binary);
 	if (myFile.is_open()) {
-		size_t len;
-		myFile.read(reinterpret_cast<char*>(&len), sizeof(size_t));
-		int* vec = new int[len];
-		myFile.read(reinterpret_cast<char*>(&vec), len);
-		for (int i = 0; i < len; i++)
-			o[i] = vec[i];
-		delete[] vec;
+		int len;
+		myFile.read(reinterpret_cast<char*>(&len), sizeof(int));
+		o.resize(len);
+		for (int i = 0; i < len; i++) {
+			int aux;
+			myFile.read(reinterpret_cast<char*>(&aux), sizeof(int));
+			o[i] = aux;
+		}
+	}
+	myFile.close();
+}
+
+void Save(std::vector<Object> o, std::string fileName) {
+	std::ofstream myFile;
+	myFile.open(fileName, std::ios::binary);
+	if (myFile.is_open()) {
+		int len = o.size();
+		myFile.write(reinterpret_cast<char*>(&len), sizeof(int));
+		for (int i = 0; i < o.size(); i++)
+			myFile.write(reinterpret_cast<char*>(&o[i]), sizeof(Object));
+	}
+	myFile.close();
+}
+
+void Recover(std::vector<Object> &o, std::string fileName) {
+	std::ifstream myFile;
+	myFile.open(fileName, std::ios::binary);
+	if (myFile.is_open()) {
+		int len;
+		myFile.read(reinterpret_cast<char*>(&len), sizeof(int));
+		o.resize(len);
+		for (int i = 0; i < len; i++) {
+			Object aux;
+			myFile.read(reinterpret_cast<char*>(&aux), sizeof(Object));
+			o[i] = aux;
+		}
 	}
 	myFile.close();
 }
 
 int main() {
+	//Ex 1
 	std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
 	SaveVector(vec, "lol.txt");
-
 	std::vector<int> vec2;
 	RecoverVector(vec2, "lol.txt");
-
 	for (int i = 0; i < vec2.size(); i++)
-		std::cout << vec2[i] << std::endl;
+		std::cout << vec2[i] << " ";
+	std::cout << std::endl;
+	//Ex 2
+	std::vector<Object> objects = { {'c', 6, 9} , {'z', 48, 10} };
+	Save(objects, "objects.txt");
+	std::vector<Object> objects2;
+	Recover(objects2, "objects.txt");
+	for (int i = 0; i < objects2.size(); i++)
+		std::cout << objects2[i].type << " " << objects2[i].x << " " << objects2[i].y << std::endl;
+
 
 	return 0;
 }
