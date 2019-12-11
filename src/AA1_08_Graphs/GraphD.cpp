0a48eb21d604c97cdfac94674c32a0522d1749b0
Adrià Jiménez Chincolla <adrianjimenezchincolla@gmail.com>
2019-12-11 19:10:33 +0100

    Finished GraphD

--- src/AA1_08_Graphs/GraphD.cpp
+++ src/AA1_08_Graphs/GraphD.cpp
@@ -19,20 +19,29 @@ void GraphD::Insert(edge e) {
 	}
 	else { // if node already exists in the map
 		std::vector<vertex>::iterator it = find(tmp->second.begin(), tmp->second.end(), e.second);
-		if (it != tmp->second.end()) // if node already exists in vector
+		if (it == tmp->second.end()) // if node doesn't exists in the vector
 			tmp->second.push_back(e.second);
 	}
+
+	tmp = g.find(e.second);
+	if (tmp == g.end()) // if second node doesn't exist in the map
+		g.insert({ e.second, std::vector<vertex>() });
 }
 
 void GraphD::Remove(edge e) {
-
+	std::unordered_map<vertex, std::vector<vertex>>::iterator tmp = g.find(e.first);
+	if (tmp != g.end()) { // if node exists in the map
+		std::vector<vertex>::iterator it = find(tmp->second.begin(), tmp->second.end(), e.second);
+		if (it != tmp->second.end()) // if node exists in the vector
+			tmp->second.erase(it);
+	}
 }
 
 void GraphD::Print() {
 	for (std::unordered_map<vertex, std::vector<vertex>>::iterator it = g.begin(); it != g.end(); it++) {
 		std::cout << it->first << "-> ";
 		for (std::vector<vertex>::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++) {
-			std::cout << *it2;
+			std::cout << *it2 << ", ";
 		}
 		std::cout << std::endl;
 	}
--- src/AA1_08_Graphs/main.cpp
+++ src/AA1_08_Graphs/main.cpp
@@ -3,9 +3,15 @@
 int main() {
 	GraphD g;
 	g.Insert({ 1,2 });
-	g.Insert({ 3,1 });
+	g.Insert({ 3,4 });
 	g.Insert({ 2,2 });
-	g.Insert({ 4,2 });
+	g.Insert({ 2,2 });
+	g.Insert({ 2,3 });
+
+	g.Remove({ 2,3 });
+	g.Remove({ 2,1 });
+	g.Remove({ 5,2 });
+
 	g.Print();
 	return 0;
 }
